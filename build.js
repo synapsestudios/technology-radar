95d59050ad44095e0048bdfe1571a555f52a4db8
Aaron Bruce <aaron@synapsestudios.com>
2023-03-15 15:18:25 -0700

    allow for "publish" flag

--- build.js
+++ build.js
@@ -12,13 +12,14 @@ const getFiles = (dir) => new Promise((resolve, reject) => {
 async function main() {
 	try {
 		const files = await getFiles('./blips');
-		const results = await Promise.all(files.map(async f => {
+		const results = (await Promise.all(files.map(async f => {
 			const data = await fs.promises.readFile(`./blips/${f}`, 'utf8');
 			var md = new MarkdownIt();
 			md.use(meta);
 			var html = md.render(data);
-			return {...md.meta, description: html, isNew: md.meta.isNew ? 'TRUE' : 'FALSE'}
-		}))
+			if (md.meta.published === false) return null
+			return {name: md.meta.name, quadrant: md.meta.quadrant, ring: md.meta.ring, description: html, isNew: md.meta.isNew ? 'TRUE' : 'FALSE'}
+		}))).filter(v => v !== null);
 
 		await fs.promises.writeFile('./test.json', JSON.stringify(results))
 
