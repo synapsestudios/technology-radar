fef7b166968a608431c09a922076593bd00372f7
Aaron Bruce <aaron@synapsestudios.com>
2023-03-15 17:00:18 -0700

    refactor fs dependencies away from business logic

--- src/build.js
+++ src/build.js
@@ -9,16 +9,20 @@ const getFiles = (dir) => new Promise((resolve, reject) => {
 	})
 })
 
-module.exports = async (opts = {dir: '', outFile: ''}) =>  {
-	const files = await getFiles(opts.dir);
-	const results = (await Promise.all(files.map(async f => {
-		const data = await fs.promises.readFile(`${opts.dir}/${f}`, 'utf8');
+const parse = async (files, readData) => {
+	return (await Promise.all(files.map(async f => {
+		const data = await readData(f);
 		var md = new MarkdownIt();
 		md.use(meta);
 		var html = md.render(data);
 		if (md.meta.published === false) return null
 		return {name: md.meta.name, quadrant: md.meta.quadrant, ring: md.meta.ring, description: html, isNew: md.meta.isNew ? 'TRUE' : 'FALSE'}
 	}))).filter(v => v !== null);
+}
 
+module.exports = async (opts = {dir: '', outFile: ''}) =>  {
+	const files = await getFiles(opts.dir);
+	const readData = (f) => fs.promises.readFile(`${opts.dir}/${f}`, 'utf8')
+	return parse(files, readData);
 	await fs.promises.writeFile(opts.outFile, JSON.stringify(results))
 }
